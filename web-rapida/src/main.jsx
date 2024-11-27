99bff363092cea6120f76ab16d8acdea1e35a0b9
markhitos <mc2622016@gmail.com>
2024-11-27 19:18:47 -0300

    arreglando errores 4

--- web-rapida/src/main.jsx
+++ web-rapida/src/main.jsx
@@ -2,10 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './App.jsx'
+import Home from './Home.jsx'
+import Service from './Service.jsx'
+import Enlaces from './Enlaces.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-  
+     <Home/>
+     <Service/>
+     <Enlaces/>
   </StrictMode>,
 )
