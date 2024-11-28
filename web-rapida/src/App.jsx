3c7c362784ad22ab6db48e346c7db813486e78c0
markhitos <mc2622016@gmail.com>
2024-11-28 00:21:18 -0300

    cargando vercel.json

--- web-rapida/src/App.jsx
+++ web-rapida/src/App.jsx
@@ -1,10 +1,16 @@
 
 import Navbar from "./Navbar"
+import Home from "./Home"
+import Service from "./Service"
+import Enlaces from "./Enlaces"
 
-export default function Header() {
+export default function App() {
   return (
    <div  className="w-full h-full absolute bg-gradient-to-r from-blue-300 to-violet-700" >
      <Navbar/>
+     <Home/>
+     <Service/>
+     <Enlaces/>
    </div>
   )
 }
--- web-rapida/src/main.jsx
+++ web-rapida/src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './App.jsx'
-import Home from './Home.jsx'
-import Service from './Service.jsx'
-import Enlaces from './Enlaces.jsx'
-
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-     <Home/>
-     <Service/>
-     <Enlaces/>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 )
